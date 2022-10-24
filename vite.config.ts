39157e10448d7b8577e0c7cadc2ef1c9cc5c10c6
zhengzhicong <zhengzhicong@mokahr.com>
2022-10-24 17:21:30 +0800

    add env config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig( ({command, mode}) => {
+  const envs = loadEnv(mode, process.cwd())
+  return {
+    server: {
+        port: Number.parseInt(envs.VITE_PORT),
+        
+    },
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+          "@": path.resolve(__dirname, "/src"),
+          "~@": path.resolve(__dirname, "/src"),
+      },
+    },
   }
 })
