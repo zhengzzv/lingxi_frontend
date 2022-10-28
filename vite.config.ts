42fd8f590fab6f15eb95de1a5c9dd21ef52fa6c6
zhengzzv <1064275075@qq.com>
2022-10-28 11:41:54 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import * as path from "path";
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const envs = loadEnv(mode, process.cwd());
